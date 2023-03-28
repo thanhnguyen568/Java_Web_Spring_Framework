7b5b3944baac3b3f452a3e7192f22458e0767007
thanhnguyen568 <thanhnguyen230@gail.com>
2023-03-28 09:32:29 +0700

    update ss

--- demo/SpringDemo/src/main/java/codegym/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package codegym.repository;
-
-import codegym.model.Category;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CategoryRepository extends CrudRepository<Category, Integer> {
-}
