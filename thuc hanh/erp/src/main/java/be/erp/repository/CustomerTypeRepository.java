4124d9038195228a13c6836ef19459d8eca07f9f
thanhnguyen568 <thanhnguyen230@gmail.com>
2023-04-07 20:19:05 +0700

    update ss

--- thuc hanh/erp/src/main/java/be/erp/repository/CustomerTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package be.erp.repository;
-
-import be.erp.entity.CustomerType;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerTypeRepository extends PagingAndSortingRepository<CustomerType, Long> {
-}
